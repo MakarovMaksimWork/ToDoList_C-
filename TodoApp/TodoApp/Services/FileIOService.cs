fdd1856ef6923b26e8d4448971bed75155f6bb52
Maksim <122198840+MakarovMaksimWork@users.noreply.github.com>
2023-04-04 19:07:15 +0300

    ToDoList_for_Windows_Csharp
    Just a simple example

---
+++ TodoApp/TodoApp/Services/FileIOService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Models;
+
+namespace TodoApp.Services
+{
+    internal class FileIOService
+    {
+
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+
+        public BindingList<TodoModel> LoadData()
+        {
+           var fileExist = File.Exists(PATH);
+           if (!fileExist)
+           {
+               File.CreateText(PATH).Dispose();
+               return new BindingList<TodoModel>();
+           }
+           using (var reader = File.OpenText(PATH))
+           {
+               var fileText = reader.ReadToEnd();
+               return JsonConvert.DeserializeObject<BindingList<TodoModel>>(fileText);
+           }
+        }
+
+        public void SaveData(object todoDataList) 
+        {
+            using (StreamWriter Writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(todoDataList);
+                Writer.Write(output);
+            }
+        }
+    }
+}
